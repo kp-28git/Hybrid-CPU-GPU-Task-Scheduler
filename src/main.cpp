28bdd78a75a1563686866f10f6d4cd2ae666fb25
kushal <dtpatel393@gmail.com>
2025-08-16 15:59:46 +0530

    main: Run matrix multplication using GPU

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include "cpu_tasks.h"
+#include "gpu_tasks.h"
 
 int main() {
     std::cout << "=== CPU Tasks Demo ===" << std::endl;
@@ -13,12 +14,19 @@ int main() {
     // std::cout << "Gaussian blur done. Output saved as blurred.ppm\n";
 
     // 2. Matrix multiplication
-    size_t N = 5; // Size of the matrices
+    // size_t N = 5; // Size of the matrices
+    // cpu_tasks cpuObj;
+    // cpuObj.matrixMultiplyCPU(N);
 
 
+    size_t N = 4; // Size of the matrices
+    gpu_tasks gpuMat(N);
 
-    cpu_tasks cpuObj;
-    cpuObj.matrixMultiplyCPU(N);
+    std::cout << "Running GPU Matrix Multiplication...\n";
+    gpuMat.matrixMultiplyGPU();
+
+    std::cout << "Result Matrix C:\n";
+    gpuMat.printResult();
 
     return 0;
 }
