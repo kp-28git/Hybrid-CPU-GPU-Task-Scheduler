4b4d035c0ed06ec040769640b9c5522b379cb33b
kushal <dtpatel393@gmail.com>
2025-08-19 00:33:57 +0530

    main: Run sorting task.

--- src/main.cpp
+++ src/main.cpp
@@ -20,55 +20,55 @@ int main() {
    std::vector<int> N = {2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048};
 
     task t1("MatrixMultiply-2", N[0],
-        [&]() { cpu.matrixMultiplyCPU(N[0]); },
-        [&]() {gpu.matrixMultiplyGPU(N[0]); });
+        [&]() { cpu.sortingCPU(N[0]); },
+        [&]() {gpu.sortingGPU(N[0]); });
 
     task t2("MatrixMultiply-4", N[1],
-        [&]() { cpu.matrixMultiplyCPU(N[1]); },
-        [&]() {gpu.matrixMultiplyGPU(N[1]); });
+        [&]() { cpu.sortingCPU(N[1]); },
+        [&]() {gpu.sortingGPU(N[1]); });
 
     task t3("MatrixMultiply-8", N[2],
-        [&]() { cpu.matrixMultiplyCPU(N[2]); },
-        [&]() {gpu.matrixMultiplyGPU(N[2]); });
+        [&]() { cpu.sortingCPU(N[2]); },
+        [&]() {gpu.sortingGPU(N[2]); });
     
     task t4("MatrixMultiply-16", N[3],
-        [&]() { cpu.matrixMultiplyCPU(N[3]); },
-        [&]() {gpu.matrixMultiplyGPU(N[3]); });
+        [&]() { cpu.sortingCPU(N[3]); },
+        [&]() {gpu.sortingGPU(N[3]); });
 
     task t5("MatrixMultiply-32", N[4],
-        [&]() { cpu.matrixMultiplyCPU(N[4]); },
-        [&]() {gpu.matrixMultiplyGPU(N[4]); });
+        [&]() { cpu.sortingCPU(N[4]); },
+        [&]() {gpu.sortingGPU(N[4]); });
 
     task t6("MatrixMultiply-64", N[5],
-        [&]() { cpu.matrixMultiplyCPU(N[5]); },
-        [&]() {gpu.matrixMultiplyGPU(N[5]); });
+        [&]() { cpu.sortingCPU(N[5]); },
+        [&]() {gpu.sortingGPU(N[5]); });
 
     task t7("MatrixMultiply-128", N[6],
-        [&]() { cpu.matrixMultiplyCPU(N[6]); },
-        [&]() {gpu.matrixMultiplyGPU(N[6]); });
+        [&]() { cpu.sortingCPU(N[6]); },
+        [&]() {gpu.sortingGPU(N[6]); });
 
     task t8("MatrixMultiply-256", N[7],
-        [&]() { cpu.matrixMultiplyCPU(N[7]); },
-        [&]() {gpu.matrixMultiplyGPU(N[7]); });
+        [&]() { cpu.sortingCPU(N[7]); },
+        [&]() {gpu.sortingGPU(N[7]); });
 
     task t9("MatrixMultiply-512", N[8],
-        [&]() { cpu.matrixMultiplyCPU(N[8]); },
-        [&]() {gpu.matrixMultiplyGPU(N[8]); });
+        [&]() { cpu.sortingCPU(N[8]); },
+        [&]() {gpu.sortingGPU(N[8]); });
 
     task t10("MatrixMultiply-1024", N[9],
-        [&]() { cpu.matrixMultiplyCPU(N[9]); },
-        [&]() {gpu.matrixMultiplyGPU(N[9]); });
+        [&]() { cpu.sortingCPU(N[9]); },
+        [&]() {gpu.sortingGPU(N[9]); });
 
     scheduler.addTask(t1);
     scheduler.addTask(t2);
-    // scheduler.addTask(t3);
-    // scheduler.addTask(t4);
-    // scheduler.addTask(t5);
-    // scheduler.addTask(t6);
-    // scheduler.addTask(t7);
-    // scheduler.addTask(t8);
-    // scheduler.addTask(t9);
-    // scheduler.addTask(t10);
+    scheduler.addTask(t3);
+    scheduler.addTask(t4);
+    scheduler.addTask(t5);
+    scheduler.addTask(t6);
+    scheduler.addTask(t7);
+    scheduler.addTask(t8);
+    scheduler.addTask(t9);
+    scheduler.addTask(t10);
     scheduler.run();
     
     timer.stop();
