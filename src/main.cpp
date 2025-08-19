73994ae79c44af73e4b3df0f97bface379d3a483
kushal <dtpatel393@gmail.com>
2025-08-20 00:21:48 +0530

    main: Minor Refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,54 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include "cpu_tasks.h"
-#include "gpu_tasks.h"
+
 #include "metrics.h"
 #include "scheduler.h"
 #include "task.h"
 
 int main() {
-
-    cpu_tasks cpu;
-    gpu_tasks gpu;
-
     metrics timer;
     timer.start();
 
     // Create scheduler with a policy
-    scheduler scheduler(Policy::ROUND_ROBIN, 5000);
+    scheduler scheduler(Policy::SOLE_CPU, 5000);
 
-   std::vector<int> N = {2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048};
+   std::vector<int> N = {2, 4, 8, 16, 32, 64, 128, 256, 512, 1024};
 
-    task t0(operation::SORTING, N[0]);
+    task t0(operation::MATRIX_MULTIPLY, N[0]);
     scheduler.addTask(t0);
 
-    task t1(operation::SORTING, N[1]);
+    task t1(operation::MATRIX_MULTIPLY, N[1]);
     scheduler.addTask(t1);
 
-    task t2(operation::SORTING, N[2]);
+    task t2(operation::MATRIX_MULTIPLY, N[2]);
     scheduler.addTask(t2);
 
-    task t3(operation::SORTING, N[3]);
+    task t3(operation::MATRIX_MULTIPLY, N[3]);
     scheduler.addTask(t3);
 
     task t4(operation::MATRIX_MULTIPLY, N[4]);
     scheduler.addTask(t4);
 
+    task t5(operation::MATRIX_MULTIPLY, N[5]);
+    scheduler.addTask(t5);
+
+    task t6(operation::MATRIX_MULTIPLY, N[6]);
+    scheduler.addTask(t6);
+
+    task t7(operation::MATRIX_MULTIPLY, N[7]);
+    scheduler.addTask(t7);
+
+    task t8(operation::MATRIX_MULTIPLY, N[8]);
+    scheduler.addTask(t8);
+
+    task t9(operation::MATRIX_MULTIPLY, N[9]);
+    scheduler.addTask(t9);
+
     scheduler.run();
     
+    std::cout << "\n\nTotal execution time : ";
     timer.stop();
-    std::cout << "\n\nTotal execution time: " << timer.elapsedMs() << "\n";
+    // std::cout << "\n\nTotal execution time: " << timer.elapsedMs() << "\n";
     return 0;
 }
